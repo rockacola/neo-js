7a869722cca22b367e953568a8999288846f6da1
Travis <travis@localhost.dev>
2018-08-19 23:04:38 +1000

    Scaffold Mesh core class

---
+++ src/core/mesh.ts
@@ -0,0 +1,30 @@
+import { EventEmitter } from "events"
+import { Logger, LoggerOptions } from 'node-log-it'
+import { merge } from 'lodash'
+import { RpcDelegate } from '../delegates/rpc-delegate'
+
+interface Options {
+  loggerOptions?: LoggerOptions,
+}
+
+const MODULE_NAME = 'Mesh'
+const DEFAULT_OPTIONS: Options = {
+  loggerOptions: {},
+}
+
+export class Mesh extends EventEmitter {
+  private options: Options
+  private logger: Logger
+
+  constructor(options = {}) {
+    super()
+
+    // Associate optional properties
+    this.options = merge({}, DEFAULT_OPTIONS, options)
+
+    // Bootstrapping
+    this.logger = new Logger(MODULE_NAME, this.options.loggerOptions)
+
+    this.logger.debug('constructor completes.')
+  }
+}
