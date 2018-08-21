59c406a48239eba9a553b7366773c19cacd398e8
Travis <travis@localhost.dev>
2018-08-21 15:15:33 +1000

    Further implement getRandomNode in mesh

--- src/core/mesh.ts
+++ src/core/mesh.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from "events"
 import { Logger, LoggerOptions } from 'node-log-it'
-import { merge } from 'lodash'
+import { merge, filter } from 'lodash'
+import { Chance } from 'chance'
 import { Node } from "./node"
 
+const chance = new Chance()
 const MODULE_NAME = 'Mesh'
 const DEFAULT_OPTIONS: MeshOptions = {
   loggerOptions: {},
@@ -13,7 +15,7 @@ export interface MeshOptions {
 }
 
 export class Mesh extends EventEmitter {
-  public nodes: Node[]
+  public nodes: Node[] // Ensure there's at least 1 item in the array
   private options: MeshOptions
   private logger: Logger
 
@@ -22,6 +24,9 @@ export class Mesh extends EventEmitter {
 
     // Associate required properties
     this.nodes = nodes
+    if (this.nodes.length === 0) {
+      throw new Error('Mesh must have 1 or more nodes.')
+    }
 
     // Associate optional properties
     this.options = merge({}, DEFAULT_OPTIONS, options)
@@ -30,5 +35,31 @@ export class Mesh extends EventEmitter {
     this.logger = new Logger(MODULE_NAME, this.options.loggerOptions)
 
     this.logger.debug('constructor completes.')
+  }
+
+  // getFastestNode(): Node {
+  // }
+
+  // getHighestNode(): Node {
+  // }
+
+  /**
+   * @param activeOnly Toggle to only pick node that is determined to be active.
+   */
+  getRandomNode(activeOnly = true): Node | undefined {
+    this.logger.debug('getRandomNode triggered.')
+
+    if (!activeOnly) { // Pick any from all nodes
+      const randomIndex = chance.natural({ min: 0, max: this.nodes.length-1 })
+      return this.nodes[randomIndex]
+    }
+
+    const activeNodes = filter(this.nodes, { isActive: true })
+    if (activeNodes.length === 0) {
+      return undefined
+    }
+
+    const randomIndex = chance.natural({ min: 0, max: activeNodes.length-1 })
+    return this.nodes[randomIndex]
   }
 }
