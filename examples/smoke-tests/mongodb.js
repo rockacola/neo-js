5f8cc09bae0373b597eb790ca9dc8217d6930fcf
Travis <travis@localhost.dev>
2018-08-24 14:12:42 +1000

    Scaffold mongoose usage

---
+++ examples/smoke-tests/mongodb.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const Neo = require('../../dist/neo').Neo
+
+process.on('unhandledRejection', (reason, p) => {
+  console.warn('Unhandled Rejection at: Promise', p, 'reason:', reason)
+})
+
+// -- Parameters
+
+const network = 'mainnet'
+const dbConnectOnInit = true
+const dbConnectionString = 'mongodb://localhost/neo_mainnet'
+const blockCollectionName = 'blocks'
+
+// -- Implementation
+
+;(async () => {
+  console.log('== MongoDB Example ==')
+  const neo = new Neo({
+    network: network,
+    storageType: 'mongodb',
+    storageOptions: {
+      connectOnInit: dbConnectOnInit,
+      connectionString: dbConnectionString,
+      collectionNames: {
+        blocks: blockCollectionName,
+      },
+      loggerOptions: { level: 'debug' },
+    },
+    meshOptions: {
+      loggerOptions: { level: 'debug' },
+    },
+    apiOptions: {
+      loggerOptions: { level: 'debug' },
+    },
+    loggerOptions: { level: 'debug' },
+  })
+
+  // Wait for mesh to be ready before start testing
+  neo.mesh.on('ready', async () => {
+    const blockCount = await neo.api.getBlockCount()
+    console.log('=> blockCount:', blockCount)
+
+    neo.mesh.stopBenchmark()
+    console.log('=== THE END ===')
+  })
+})()
--- src/storages/mongodb-storage.ts
+++ src/storages/mongodb-storage.ts
@@ -1,13 +1,29 @@
 import { EventEmitter } from "events"
 import { Logger, LoggerOptions } from 'node-log-it'
 import { merge } from 'lodash'
+import { Mongoose } from "mongoose"
+const mongoose = new Mongoose()
+mongoose.Promise = global.Promise // Explicitly supply promise library (http://mongoosejs.com/docs/promises.html)
 
 const MODULE_NAME = 'MongodbStorage'
 const DEFAULT_OPTIONS: MongodbStorageOptions = {
+  connectOnInit: true,
+  collectionNames: {
+    blocks: 'blocks',
+    transactions: 'transactions',
+    assets: 'assets',
+  },
   loggerOptions: {},
 }
 
 export interface MongodbStorageOptions {
+  connectOnInit?: boolean,
+  connectionString?: string,
+  collectionNames?: {
+    blocks?: string,
+    transactions?: string,
+    assets?: string,
+  },
   loggerOptions?: LoggerOptions,
 }
 
@@ -24,13 +40,37 @@ export class MongodbStorage extends EventEmitter {
 
     // Bootstrapping
     this.logger = new Logger(MODULE_NAME, this.options.loggerOptions)
-    this._isReady = true
+    // TODO: init models
+    this.initConnection()
+    this.logger.debug('mongoose:', mongoose)
 
     this.logger.debug('constructor completes.')
   }
 
   isReady(): boolean {
     return this._isReady
+  }
+
+  private initConnection() {
+    if (this.options.connectOnInit) {
+      this.logger.debug('initConnection triggered.')
+      // TODO: valid connection string
+
+      mongoose.connect(this.options.connectionString!, { useMongoClient: true })
+        .then(() => {
+          this.setReady()
+          this.logger.info('mongoose connected.')
+        })
+        .catch((err: any) => {
+          this.logger.error('Error establish MongoDB connection.')
+          throw err
+        })
+    }
+  }
+
+  private setReady() {
+    this._isReady = true
+    this.emit('ready')
   }
 
   getBlockCount(): Promise<number> {
