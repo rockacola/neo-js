f7c104b03a0d2744e2d6bd9267bf1cdbb110d1c2
Travis <travis@localhost.dev>
2018-09-02 23:42:28 +1000

    Update processed files

--- dist/neo.d.ts
+++ dist/neo.d.ts
@@ -4,6 +4,7 @@ import { LoggerOptions } from 'node-log-it';
 import { Mesh, MeshOptions } from './core/mesh';
 import { NodeOptions } from './core/node';
 import { Api, ApiOptions } from './core/api';
+import { Syncer, SyncerOptions } from './core/syncer';
 import { MemoryStorage, MemoryStorageOptions } from './storages/memory-storage';
 import { MongodbStorage, MongodbStorageOptions } from './storages/mongodb-storage';
 export interface NeoOptions {
@@ -14,12 +15,14 @@ export interface NeoOptions {
     meshOptions?: MeshOptions;
     storageOptions?: MemoryStorageOptions | MongodbStorageOptions;
     apiOptions?: ApiOptions;
+    syncerOptions?: SyncerOptions;
     loggerOptions?: LoggerOptions;
 }
 export declare class Neo extends EventEmitter {
     mesh: Mesh;
     storage?: MemoryStorage | MongodbStorage;
     api: Api;
+    syncer: Syncer;
     private options;
     private logger;
     constructor(options?: NeoOptions);
@@ -28,6 +31,7 @@ export declare class Neo extends EventEmitter {
     private getMesh;
     private getStorage;
     private getApi;
+    private getSyncer;
     private getNodes;
     close(): void;
 }
